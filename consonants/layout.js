c1a2a46ccea3292920e349fc7787a252a1a8ccf4
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-20 09:24:15 -0600

    feat: add spacing and rounded to differents compoenents

---
+++ consonants/layout.js
@@ -0,0 +1,11 @@
+export const SPACING = { // Padding and margin
+    sm: 8,
+    md: 16,
+    lg: 24,
+}
+
+export const RADIUS = {
+    sm: 4, // Small
+    md: 6, // Medium
+    lg: 8, // Large
+}
