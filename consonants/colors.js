df7e4428b67a13b783b4303211956773e654fdf1
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-20 09:21:47 -0600

    feat: add colors that them will be used

---
+++ consonants/colors.js
@@ -0,0 +1,37 @@
+export const COLORS = {
+    light: {
+        primary: '#004B44',
+        primaryContainer: '#0A6F66',
+        secondary: '#005DB2',
+        tertiary: '#2E7D32',
+
+        surface: '#FEFEFE',
+        surfaceLow: '#F5F5F5',
+        surfaceHigh: '#E8E8E8',
+        surfaceHighest: '#E0E0E0',
+
+        text: '#1C1C1C',
+        textVariant: '#6B7280',
+
+        error: '#BA1A1A',
+        outline: 'rgba(0,0,0,0.15)',
+    },
+
+    dark: {
+        primary: '#66dd8b',
+        primaryContainer: '#006e36',
+        secondary: '#4DA3FF',
+        tertiary: '#66BB6A',
+
+        surface: '#0f1412',
+        surfaceLow: '#181d1a',
+        surfaceHigh: '#272d29',
+        surfaceHighest: '#313633',
+
+        text: '#dfe4e0',
+        textVariant: '#bec9c1',
+
+        error: '#ffb4ab',
+        outline: 'rgba(63,73,67,0.15)',
+    },
+}
