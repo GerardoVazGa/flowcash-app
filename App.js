36276cc92d40304573492640add780b6cbc785f9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-19 20:36:13 -0600

    feat: add home screen on aplication file

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Home} from './app/screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
+      
     </View>
   );
 }
