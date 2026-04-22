1266ce9d85ce4d9765659d53024d3175d8dcae43
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-22 14:35:03 -0600

    feat(ui): connect HomeScreen with dinamic finance data

--- src/app/screens/HomeScreen.jsx
+++ src/app/screens/HomeScreen.jsx
@@ -8,68 +8,23 @@ import { MetricCard } from "../../components/financial/MetricCard.jsx";
 import { COLORS } from "../../constants/colors.js";
 import { SPACING, RADIUS } from "../../constants/layout.js";
 import { TransactionsList } from "../../components/financial/TransactionsList.jsx";
+import { useFinances } from "../../hooks/useFinances.js";
 
 export function HomeScreen(){
 
-    const transactions = [
-        {
-            id: 1,
-            label: "Transferencia recibida",
-            category: "Transferencia",
-            account: "Cuenta 1",
-            amount: 1000,
-            type: "income",
-            date: "2026-04-21",
-        },
-        {
-            id: 2,
-            label: "Supermercado",
-            category: "Alimentación",
-            account: "Cuenta 1",
-            amount: 350,
-            type: "expense",
-            date: "2026-04-20",
-        },
-        {
-            id: 3,
-            label: "Pago de nómina",
-            category: "Trabajo",
-            account: "Cuenta 1",
-            amount: 5000,
-            type: "income",
-            date: "2026-04-19",
-        },
-        {
-            id: 4,
-            label: "Transporte público",
-            category: "Transporte",
-            account: "Cuenta 1",
-            amount: 45,
-            type: "expense",
-            date: "2026-04-19",
-        },
-        {
-            id: 5,
-            label: "Suscripción Netflix",
-            category: "Entretenimiento",
-            account: "Cuenta 1",
-            amount: 199,
-            type: "expense",
-            date: "2026-04-18",
-        },
-    ]
-    
+    const {transactions, incomes, expenses, totalBalance} = useFinances()
     
     return (
         <ScrollView 
             style={styles.container}
             contentContainerStyle={styles.content}
+            showsVerticalScrollIndicator={false}
         >
-            <BalanceCard/>
+            <BalanceCard totalBalance={totalBalance.toFixed(2)}/>
             <Section title="Metricas" style={styles.metricSection}>
                 <MetricCard 
                     label="Ingresos" 
-                    value="$124,560.85" 
+                    value={incomes.toFixed(2)} 
                     icon= {
                             <AppIcon 
                                 name="trending-up-outline" 
@@ -83,7 +38,7 @@ export function HomeScreen(){
                 />
                 <MetricCard 
                     label="Gastos" 
-                    value="$124,560.85" 
+                    value={expenses.toFixed(2)} 
                     icon= {
                             <AppIcon 
                                 name="trending-down-outline" 
