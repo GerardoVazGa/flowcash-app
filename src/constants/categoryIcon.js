2b16363ccd829a7b2e12f5d88a83b3ab52950709
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-22 14:07:46 -0600

    chore(constants): add icon map for financial data

---
+++ src/constants/categoryIcon.js
@@ -0,0 +1,10 @@
+export const CATEGORY_ICONS = {
+    Trabajo: "briefcase-outline",
+    Alimentación: "fast-food-outline",
+    Transporte: "car-outline",
+    Entretenimiento: "film-outline",
+    Servicios: "wifi-outline",
+    Salud: "medical-outline",
+    Ventas: "cash-outline",
+    Transferencia: "swap-horizontal-outline",
+};
