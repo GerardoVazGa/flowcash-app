03169c3b389a286487ea585a131dd602fd0172f6
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-22 14:12:06 -0600

    feat(finance): add useFinance hook for handling transactions data

---
+++ src/hooks/useTransactions.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+import { transactionsService } from "../services/transactionsService.js"
+
+export const useTransactions = () => {
+    const  transactions = transactionsService().getTransactions()
+    
+    const incomes = transactions.filter(transaction => transaction.type === "income").reduce((totalIncomes, transaction) => totalIncomes + transaction.amount, 0)
+    const expenses = transactions.filter(transaction => transaction.type === "expense").reduce((totalExpenses, transaction) => totalExpenses + transaction.amount, 0)
+
+    const totalBalance = incomes - expenses
+
+    return {
+        transactions,
+        incomes,
+        expenses,
+        totalBalance
+    }
+}
