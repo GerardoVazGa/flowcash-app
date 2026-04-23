d092f7f6f6073b50a38c80d563425c4c790f7b63
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-23 16:52:41 -0600

    chore(constants): add status config for usage diferent status variants

---
+++ src/constants/statusConfig.js
@@ -0,0 +1,23 @@
+export const STATUS_CONFIG = {
+    safe: {
+        bar: "income",
+        bg: "incomeContainer",
+        text: "incomeStrong",
+    },
+    caution: {
+        bar: "warning",
+        bg: "warningContainer",
+        text: "warningStrong",
+    },
+    warning: {
+        bar: "orange",
+        bg: "orangeContainer",
+        text: "orangeStrong",
+    },
+    danger: {
+        bar: "expenses",
+        bg: "expensesContainer",
+        text: "expensesStrong",
+    },
+
+}
