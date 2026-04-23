9799f3c8724caf4b0a741cfb58abc2ee3fabd656
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-23 13:17:44 -0600

    feat(ui-components): add IconButton component with size system and theme tokens

---
+++ src/components/ui/IconButton.jsx
@@ -0,0 +1,57 @@
+import { Pressable, StyleSheet } from "react-native";
+import THEME from "../../constants/theme";
+import { ICON_BUTTON_SIZES } from "../../constants/buttons";
+import { AppIcon } from "./AppIcon";
+
+export function IconButton({
+    icon,
+    size = "md",
+    background = "primaryContainer",
+    colorIcon,
+    iconSize,
+    onPress,
+    disabled = false,
+    style
+}) {
+    const currentTheme = THEME.light
+
+    const backgroundStyle = disabled 
+        ? currentTheme.colors.surfaceHigh
+        : currentTheme.colors?.[background] || currentTheme.colors.primaryContainer 
+
+    const iconColorStyle = currentTheme.colors?.[colorIcon] || currentTheme.colors.surface
+    return (
+        <Pressable
+            disabled = {disabled}
+            onPress={onPress}
+            style={
+                [
+                    styles.container,
+                    { 
+                        width: ICON_BUTTON_SIZES[size].size,
+                        height: ICON_BUTTON_SIZES[size].size,
+                        borderRadius: currentTheme.radius.md,
+                        backgroundColor: backgroundStyle,
+                        opacity: disabled ? 0.5 : 1
+                    },
+                    style
+                ]
+            }
+        >
+            <AppIcon 
+                name={icon}
+                color={iconColorStyle}
+                size={ICON_BUTTON_SIZES[size].iconSize || ICON_BUTTON_SIZES.sm.iconSize}
+            />
+
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        overflow: 'hidden',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
