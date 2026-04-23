f1668acd4241669f2ef9c7c458771b25d70e190a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-22 18:45:51 -0600

    refactor(layout-components): delete prop title and component AppText from Section layout component

--- src/components/layouts/Section.jsx
+++ src/components/layouts/Section.jsx
@@ -1,13 +1,9 @@
 import { View } from "react-native";
 import { AppText } from "../ui/AppText";
 
-export function Section({title, children, style}) {
+export function Section({children, style}) {
     return (
         <View> 
-            {title && (
-                <AppText variant="headline">{title}</AppText>
-                )
-            }
             <View style={style}>{children}</View>
         </View>
     )
