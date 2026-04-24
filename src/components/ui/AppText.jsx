c8c39e5855b197673eb6323be1a3f10f0117408d
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-23 19:42:41 -0600

    refactor(ui-components): Use useTheme hook in AppText ui component

--- src/components/ui/AppText.jsx
+++ src/components/ui/AppText.jsx
@@ -1,14 +1,14 @@
 import { Text, StyleSheet } from "react-native";
-import { COLORS } from "../../constants/colors.js";
-import { Typography } from "../../constants/typography.js";
+import { useTheme } from "../../hooks/useTheme.js";
 
 export function AppText({children, variant = 'display', color = 'black', style}){
+    const { theme } = useTheme()
     return(
         <Text 
             style={
                 [
-                    Typography[variant], 
-                    {color: COLORS.light[color] || color},
+                    theme.typography[variant], 
+                    {color: theme.colors[color] || color},
                     style
                 ]
             }
