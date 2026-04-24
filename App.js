0c98e3b341e2a9c5150d40de66c7150a8275bf16
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-23 18:02:26 -0600

    feat(context): Implement ThemeProvider in App component

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {HomeScreen} from './src/app/screens/HomeScreen';
+import { ThemeProvider } from './src/context/ThemeContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <HomeScreen />
-    </View>
+    <ThemeProvider>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <HomeScreen />
+      </View>
+    </ThemeProvider>
   );
 }
 
