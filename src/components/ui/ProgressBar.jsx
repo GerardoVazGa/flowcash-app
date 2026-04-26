a5c53957e71938d8f4c2785cdf81f534d52b691e
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-26 11:46:59 -0600

    feat(ui-components): add animated bar in progress bar component

--- src/components/ui/ProgressBar.jsx
+++ src/components/ui/ProgressBar.jsx
@@ -1,27 +1,45 @@
 import { StyleSheet, View } from "react-native";
 import { useTheme } from "../../hooks/useTheme";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { useEffect } from "react";
 
-export function ProgressBar({percent = 10, color}) {
+export function ProgressBar({percent = 10, color, isVisible = true}) {
     const {theme} = useTheme()
+    const progress = useSharedValue(0)
 
     const styles = getStyles(theme)
 
     const safePercent = Math.min(Math.max(percent, 0), 100)
 
+    useEffect(() => {
+        if (isVisible) {
+            progress.value = withTiming(safePercent, {duration: 2000})
+        }
+
+        return () => {
+            progress.value = 0
+        }
+    }, [isVisible, safePercent])
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            width: `${progress.value}%`
+        }
+    })
     return (
         <View style={styles.track}>
-            <View 
+            <Animated.View 
                 style={
                     [
                         styles.progressFill,
-                        {
-                            width: `${safePercent}%`, 
+                        animatedStyle,
+                        { 
                             backgroundColor: color || theme.colors.income
                         }
                     ]
                 }
             >
-            </View>
+            </Animated.View>
         </View>
     )
 }
