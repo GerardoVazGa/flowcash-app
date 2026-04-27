7d1b9274f8848fede2498fc7717b24c580ff9e5a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-26 19:55:49 -0600

    feat(app-navigation): add AppNavigator with NavigationContainer

---
+++ src/app/navigation/AppNavigator.jsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { RootStack } from "./RootStack";
+
+export function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <RootStack />
+        </NavigationContainer>
+    )
+}
