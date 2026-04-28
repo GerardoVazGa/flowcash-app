1180c8017c979a72f7513b2124d29f5dd3994752
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-28 11:57:27 -0600

    feat(app-screens): implement header with search bar and filters button and include the bottom sheet modal with the content

--- src/app/screens/TransactionsScreen.jsx
+++ src/app/screens/TransactionsScreen.jsx
@@ -1,17 +1,62 @@
 import { StyleSheet, Text, View } from "react-native";
 import { TransactionsSummary } from "../../components/financial/TransactionsSummary";
 import { useTheme } from "../../hooks/useTheme";
+import { SearchBar } from "../../components/ui/SearchBar";
+import { useState, useMemo, useRef } from "react";
+import { FiltersSheet } from "../../components/ui/FiltersSheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { IconButton } from "../../components/ui/IconButton";
 
 export function TransactionsScreen() {
+    const [text, setText] = useState("")
+    const modalRef = useRef(null)
+    const snapPoints = useMemo(() => ["50%", "75%"], [])
+    const [filters, setFilters] = useState({
+        type: "income",
+        category: "all",
+        period: {
+            type: "month",
+            value: 1
+        }
+    })
+
     const {theme} = useTheme()
     const styles = getStyles(theme)
+
+    const handleChangeText = (text) => setText(text)
+
+    const handleOpenFilters = () => {
+        console.log(modalRef.current);
+        modalRef.current?.present()
+    }
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
+                <SearchBar value={text} onChange={handleChangeText} />
+                <IconButton 
+                    icon="filter-outline" 
+                    background="transparent"
+                    colorIcon="primary"
+                    onPress={handleOpenFilters}
+                />
+            </View>
             <TransactionsSummary 
                 income={1000}
                 expense={500}
                 balance={500}
             />
+
+            <BottomSheetModal
+                ref={modalRef}
+                snapPoints={snapPoints}
+                enablePanDownToClose
+                enableDynamicSizing = {false}
+            >
+                <FiltersSheet 
+                    filters={filters} 
+                    setFilters={setFilters}
+                />
+            </BottomSheetModal>
 
         </View>
     )
@@ -19,10 +64,18 @@ export function TransactionsScreen() {
 
 const getStyles = (theme) => StyleSheet.create({
     container: {
-        flex1: 1,
+        flex: 1,
         backgroundColor: theme.colors.surface,
-        padding: theme.spacing.lg,
+        padding: theme.spacing.md,
         gap: theme.spacing.lg,
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: theme.spacing.xs,
+        paddingVertical: theme.spacing.sm,
+        gap: theme.spacing.md,
+        
     }
 })
 
