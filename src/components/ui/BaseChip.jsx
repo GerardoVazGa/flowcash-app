8c064453341d908a8e5019234ed0843d1e3293ac
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-29 15:57:04 -0600

    feat(ui-components): add base chip for usage like core to other chips

---
+++ src/components/ui/BaseChip.jsx
@@ -0,0 +1,35 @@
+import { Pressable, StyleSheet, View } from "react-native";
+import { AppText } from "./AppText";
+import { useTheme } from "../../hooks/useTheme";
+
+export function BaseChip({label, selected = false, onPress, leftIcon, rightIcon}) {
+    const { theme } = useTheme()
+    const styles = getStyles(theme, selected)
+
+    return (
+        <Pressable 
+            onPress={onPress} 
+            style= {
+                ({pressed}) => [styles.container, pressed && {opacity: 0.5}]
+            }
+        >
+            {leftIcon && <View>{leftIcon}</View>}
+            <AppText variant="body" color={selected ? "primary": "textVariant"}>{label}</AppText>
+            {rightIcon && <View>{rightIcon}</View>}
+        </Pressable>
+    )
+}
+
+const getStyles = (theme, selected) => StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: theme.spacing.xs,
+        paddingVertical: theme.spacing.xs,
+        paddingHorizontal: theme.spacing.md,
+        backgroundColor: selected ? `${theme.colors.primary}10` : theme.colors.surfaceLow,
+        borderRadius: theme.radius.full,
+        borderColor: selected ? `${theme.colors.primary}40` : theme.colors.outline,
+        borderWidth: selected ? 2 : 1
+    }
+})
