b39249ab09cb0f18ca155632f76fe76c3b8d9040
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-29 15:06:56 -0600

    feat(components): add toggle for category filter and include some filters content like options or categories

--- src/components/ui/FiltersSheet.jsx
+++ src/components/ui/FiltersSheet.jsx
@@ -1,19 +1,104 @@
 import { BottomSheetView } from "@gorhom/bottom-sheet";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { AppText } from "./AppText";
+import { useTheme } from "../../hooks/useTheme";
+import { FilterOption } from "./FilterOption";
+import { CategoryOption } from "./CategoryOption";
 
 export function FiltersSheet({filters, setFilters}) {
+    const {theme} = useTheme()
+    const styles = getStyles(theme)
+
+    const handleCleanFilter = () => setFilters({})
+
+    const toggleCategory = (category) => {
+        setFilters((prev) => {
+            const categoryExists = prev.category.includes(category)
+
+            return {
+                ...prev,
+                category: categoryExists 
+                    ? prev.category.filter((categ) => categ !== category) 
+                    : [...prev.category, category]
+            }
+        })
+    }
     return(
         <BottomSheetView style={styles.container}>
-            <Text>Filters</Text>
+            <View style={styles.header}>
+                <AppText variant="title">Filtrar Movimientos</AppText>
+                <Pressable onPress={handleCleanFilter}>
+                    <AppText variant="title" color="error">Limpiar</AppText>
+                </Pressable>
+            </View>
+
+            <AppText variant="label" color="textVariant">Periodo</AppText>
+
+            <View>
+                <FilterOption
+                    title="Todo el historial"
+                    subtitle="Todos los registros"
+                    iconName="list-outline"
+                    selected={filters.period.type === "all"}
+                    onPress={() =>
+                        setFilters((p) => ({
+                        ...p,
+                        period: { type: "all" }
+                        }))
+                    }
+                />
+
+                <FilterOption
+                    title="Por mes"
+                    subtitle="Por mes"
+                    iconName="calendar-outline"
+                    selected={filters.period.type === "month"}
+                    onPress={() =>
+                        setFilters((p) => ({
+                        ...p,
+                        period: { type: "month" }
+                        }))
+                    }
+                />
+                
+
+
+            </View>
+
+            <AppText variant="label" color="textVariant">Categoría</AppText>
+
+            <View>
+                <CategoryOption 
+                    title="comida"
+                    selected={filters.category.includes("comida")}
+                    onPress={() => toggleCategory('comida')}
+                />
+
+                <CategoryOption 
+                    title="trabajo"
+                    selected={filters.category.includes("trabajo")}
+                    onPress={() => toggleCategory('trabajo')}
+                />
+            </View>
+
+
         </BottomSheetView>
     )
 
 }
 
-const styles = StyleSheet.create({
+const getStyles = (theme) => StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16
+        paddingHorizontal: theme.spacing.lg,
+        paddingVertical: theme.spacing.md,
+        gap: theme.spacing.md
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: theme.spacing.md
     }
 })
 
