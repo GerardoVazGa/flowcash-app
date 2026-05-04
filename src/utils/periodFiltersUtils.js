6159db34dbf2b4828d5fd0e53d8f6061454962d4
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-04 17:15:39 -0600

    feat(utils): add periodToDateRange with presets and custom support

--- src/utils/periodFiltersUtils.js
+++ src/utils/periodFiltersUtils.js
@@ -1,4 +1,17 @@
+import { Label } from "@react-navigation/elements"
 import {TODAY, MONTHS_SHORT, currentYear, currentMonth } from "../constants/periodFilters"
+import {
+    startOfMonth,
+    endOfMonth,
+    startOfYear,
+    endOfYear,
+    subMonths,
+    subDays,
+    startOfDay,
+    endOfDay,
+    format
+} from "date-fns"
+import { es } from "date-fns/locale";
 
 export const getAvailableYears = (transactions) => {
 
@@ -20,4 +33,69 @@ export const getAvailableMonths = (selectedYear) => {
             disable: selectedYear === currentYear && index > currentMonth
         }
     })
+}
+
+export const periodToDateRange = (period) => {
+    const {preset, month, year} = period
+
+    if(preset === null){
+        if(month !== null && year !== null) {
+            const date = new Date(year, month, 1)
+            return {
+                start: startOfMonth(date), // 1st day of the month
+                end: endOfMonth(date), 
+                label: `${MONTHS_SHORT[month]} ${year}`,
+                sublabel: `${MONTHS_SHORT[month]} ${year}`
+            }
+        }
+
+        if(year !== null) {
+            const date = new Date(year, 0, 1)
+            return {
+                start: startOfYear(date), // 1st day of the month
+                end: endOfYear(date), 
+                label: `${year}`,
+                sublabel: `${year}`
+            }
+        }
+    }
+
+    switch(preset) {
+        case "this_month":
+            return {
+                start: startOfMonth(TODAY), // 1st day of the month
+                end: endOfMonth(TODAY), 
+                label: "Este mes",
+                sublabel: format(TODAY, "MMMM yyyy", { locale: es })
+            }
+        case "last_month": 
+            const last = subMonths(TODAY, 1)
+            return {
+                start: startOfMonth(last), // 1st day of the month
+                end: endOfMonth(last), 
+                label: "Anterior mes",
+                sublabel: format(last, "MMMM yyyy", { locale: es })
+            }
+        case "last_7_days":
+            return {
+                start: startOfDay(subDays(TODAY, 6)),
+                end: endOfDay(TODAY),
+                label: "Últimos 7 días",
+                sublabel:  `${format(subDays(TODAY, 6), "d MMM", { locale: es })} - ${format(TODAY, "d MMM", { locale: es })}`
+            }
+        case "last_30_days":
+            return {
+                start: startOfDay(subDays(TODAY, 29)),
+                end: endOfDay(TODAY),
+                label: "Últimos 30 días",
+                sublabel: `${format(subDays(TODAY, 29), "d MMM", { locale: es })} - ${format(TODAY, "d MMM", { locale: es })}`
+            }
+        default:
+            return {
+                start: startOfMonth(TODAY), // 1st day of the month
+                end: endOfMonth(TODAY), 
+                label: "Este mes",
+                sublabel: format(TODAY, "MMMM yyyy", { locale: es })
+            }
+    }
 }
