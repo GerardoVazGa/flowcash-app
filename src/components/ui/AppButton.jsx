5c579961532b5177da2efb64b9064de9f9f6cc61
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-05 17:01:26 -0600

    feat(ui-components): support gradient, outline and ghost variants; fullwidth and improve layout handling

--- src/components/ui/AppButton.jsx
+++ src/components/ui/AppButton.jsx
@@ -1,14 +1,18 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 import { BUTTON_SIZES } from "../../constants/buttons.js";
 import { useTheme } from "../../hooks/useTheme.js";
+import { LinearGradient } from "expo-linear-gradient";
 
 export function AppButton({
     children,
     onAction, 
     size = 'sm', 
     rounded = "sm", 
+    variant = 'filled', // filled | outlined | gradient
     backgroundColor = 'primaryContainer',
+    gradientColors,
     disabled = false,
+    fullWidth = false,
     style
 }) {
     const isDisabled = disabled
@@ -17,22 +21,76 @@ export function AppButton({
 
     const currentTheme = theme
 
+    const baseStyles = [
+        styles.button,
+        {
+            borderRadius: currentTheme.radius[rounded],
+            opacity: isDisabled ? 0.5 : 1,
+            alignSelf: fullWidth ? "stretch" : "auto",
+        },
+        style             
+    ]
+
+    if(variant === "gradient" && !isDisabled) {
+        return (
+            <Pressable 
+                onPress={onAction}
+                disabled = {isDisabled}
+                style={baseStyles}
+            >
+                <LinearGradient
+                    start={currentTheme.gradients.primary.start}
+                    end={currentTheme.gradients.primary.end} 
+                    colors={gradientColors || currentTheme.gradients.primary.colors}
+                    locations={currentTheme.gradients.primary.locations}
+                    style={
+                        [
+                            styles.gradient,
+                            BUTTON_SIZES[size],
+                            { borderRadius: currentTheme.radius[rounded] }
+                        ]
+                    }
+                >
+                    {children}
+                </LinearGradient>
+        </Pressable>
+        )
+    }
+
+    const getVariantStyles = () => {
+        switch(variant) {
+            case "outline": 
+                return {
+                    backgroundColor: 'transparent',
+                    borderColor: currentTheme.colors?.[backgroundColor] ?? currentTheme.colors.primaryContainer,
+                    borderWidth: 1
+                }
+            case "ghost":
+                return {
+                    backgroundColor: currentTheme.colors.surfaceLow,
+                }
+            case "filled":
+            default:
+                return {
+                    backgroundColor: isDisabled 
+                        ? currentTheme.colors.surfaceHigh
+                        : currentTheme.colors?.[backgroundColor] ?? currentTheme.colors.primaryContainer,
+
+                }
+        }
+    }
+
     return (
         <Pressable 
             onPress={onAction} 
             disabled={isDisabled}
-            style={[
-                styles.button,
-                BUTTON_SIZES[size],
-                {
-                    backgroundColor: isDisabled 
-                        ? currentTheme.colors.surfaceHigh
-                        : currentTheme.colors?.[backgroundColor] ?? currentTheme.colors.primaryContainer,
-                    borderRadius: currentTheme.radius[rounded],
-                    opacity: isDisabled ? 0.5 : 1,
-                },
-                style             
-            ]}
+            style={
+                [
+                    ...baseStyles, 
+                    BUTTON_SIZES[size],
+                    getVariantStyles()
+                ]
+            }
         >
             {children}
         </Pressable>
@@ -43,5 +101,12 @@ const styles = StyleSheet.create({
     button: {
         justifyContent: 'center',
         alignItems: 'center',
+        overflow: 'hidden'
+    },
+    gradient: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
     }
 })
