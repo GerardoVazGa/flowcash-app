d9c553b4efa8491fb5a0bb5c01ab5f0d5fbccb10
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-04 18:03:43 -0600

    feat(financial-components): add Months Grid for filtering and showing the months with data, the current month and selected month

---
+++ src/components/financial/filters/MonthsGrid.jsx
@@ -0,0 +1,72 @@
+import { StyleSheet, View } from 'react-native'
+import { useTheme } from '../../../hooks/useTheme.js'
+import { getAvailableMonths } from '../../../utils/periodFiltersUtils.js'
+import { useMemo } from 'react'
+import { BaseChip } from '../../ui/BaseChip.jsx'
+import { MonthsCeil } from './MonthsCeil.jsx'
+import { currentMonth, currentYear } from '../../../constants/periodFilters.js'
+import { useFilters } from '../../../context/FiltersContext.js'
+
+export function MonthsGrid({ viewYear }) {
+    const {theme} = useTheme()
+    const styles = getStyles(theme)
+    const {monthsWithData, draftFilters, setDraftFilters} = useFilters()
+
+    const { period } = draftFilters
+
+    const months = useMemo(() => {
+        return getAvailableMonths(viewYear)
+    }, [viewYear])
+
+    const isMonthSelected = (selectedMonth) => {
+        return (
+            period.preset === null &&
+            period.month === selectedMonth &&
+            period.year === viewYear
+        )
+    }
+
+    const hasData = (month, year) => {
+        return monthsWithData?.[year]?.has(month) ?? false
+    }
+
+    const handleSelectedMonth = (month) => {
+        setDraftFilters((prev) => ({
+            ...prev,
+            period: {
+                ...prev.period,
+                month,
+                year: viewYear
+            }
+        }))
+    }
+
+    return (
+        <View style={styles.container}>
+            {months.map((month) => (
+                <MonthsCeil 
+                    key={month.id}
+                    month={month.id}
+                    year={viewYear}
+                    selected={isMonthSelected(month.id)}
+                    isCurrentMonth={
+                        viewYear === currentYear &&
+                        month.id === currentMonth
+                    }
+                    hasData = {hasData(month.id, viewYear)}
+                    onPress={handleSelectedMonth}
+                    isDisabled={month.disable}
+                />
+            ))}
+        </View>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        gap: theme.spacing.sm,
+        alignItems: "center"
+    }
+})
