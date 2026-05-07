fc7a224dbc0d0064a9384dc10bcc84470fd65fb5
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-07 11:39:35 -0600

    feat(transactions-utils): add transactions grouping by date

---
+++ src/utils/transactions/groupTransactionsBydate.js
@@ -0,0 +1,34 @@
+import { format, isToday, isYesterday } from "date-fns";
+import { es } from "date-fns/locale";
+
+export function groupTransactionsByDate(transactions) {
+    const groupsTransactions = {}
+
+    transactions.forEach(transaction => {
+        const date = new Date(transaction.date)
+
+        let key
+
+        if(isToday(date)){
+            key = "Hoy"
+        } else if(isYesterday(date)) {
+            key = "Ayer"
+        } else {
+            key = format(date, "dd 'de' MMMM, yyyy", {locale: es})
+        }
+
+        if(!groupsTransactions[key]) {
+            groupsTransactions[key] = []
+        }
+
+        groupsTransactions[key].push(transaction)
+    })
+
+    const groups = Object.keys(groupsTransactions).map(key => (
+        {
+            header: key,
+            data: groupsTransactions[key]
+        }
+    ))
+
+}
