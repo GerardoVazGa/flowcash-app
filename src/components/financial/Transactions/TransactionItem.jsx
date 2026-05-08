2d77fc372b7382bd13533a58724d685fe71d415a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-08 13:04:40 -0600

    feat(financial-components): implement item variants and highlight style in TransactionItem

--- src/components/financial/Transactions/TransactionItem.jsx
+++ src/components/financial/Transactions/TransactionItem.jsx
@@ -4,7 +4,7 @@ import { AppIcon } from "../../ui/AppIcon.jsx"
 import { useTheme } from "../../../hooks/useTheme.js"
 import { formatCurrency } from "../../../utils/formatters/formatCurrency.js"
 
-export function TransactionItem({transaction}) {
+export function TransactionItem({transaction, variant = "card", isFeatured = false}) {
     const { theme } = useTheme()
 
     const styles = getStyles(theme)
@@ -12,7 +12,15 @@ export function TransactionItem({transaction}) {
     const isExpense = transaction.type === "expense";
     const amountColor = isExpense ? theme.colors.expenses : theme.colors.income;
     return (
-        <View style={styles.container}>
+        <View 
+            style={
+                [
+                    styles.container,
+                    variant === "card" ? styles.baseCard : styles.baseDense,
+                    isFeatured && styles.featured
+                ]
+            }
+        >
             <AppIcon name={transaction.icon} size={14} background="primaryContainer" color = "onPrimary" style={styles.icon}/>
             <View style={styles.info} >
                 <AppText variant="label" color="text">
@@ -35,12 +43,38 @@ const getStyles = (theme) => StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         gap: theme.spacing.md,
-        paddingVertical: theme.spacing.md,
-        paddingHorizontal: theme.spacing.xs,
     },
     icon: {
-        borderRadius: theme.radius.full,
+        borderRadius: theme.radius.md,
         padding: theme.spacing.sm,
+    },
+    baseDense: {
+        paddingVertical: theme.spacing.md,
+        paddingHorizontal: theme.spacing.xs
+    },
+    baseCard: {
+        padding: theme.spacing.md,
+        marginHorizontal: theme.spacing.sm,
+        marginVertical: theme.spacing.xs,
+        borderRadius: theme.radius.lg,
+        backgroundColor: theme.colors.surface,
+        shadowColor: theme.shadows.shadowColor,
+        shadowOffset: theme.shadows.shadowOffset,
+        shadowOpacity: theme.shadows.shadowOpacity,
+        shadowRadius: theme.shadows.shadowRadius,
+        elevation: theme.shadows.elevation
+    },
+    featured: {
+        borderWidth: 1,
+        borderColor: theme.colors.primary + "20",
+        borderLeftWidth: 3,
+        borderLeftColor: theme.colors.primary,
+        backgroundColor: theme.colors.surface,
+
+        shadowOpacity: 0.12,
+        shadowRadius: 10,
+
+        elevation: 3,
     },
     info: {
         flex: 1,
