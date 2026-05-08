e17b9f308fc0a281accf3c9a46f090269ae827e0
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-08 13:26:48 -0600

    feat(financial-components): implement dynamic styles and better structure into TransactionGroupList

--- src/components/financial/Transactions/TransactionGroupList.jsx
+++ src/components/financial/Transactions/TransactionGroupList.jsx
@@ -1,18 +1,40 @@
-import { SectionList } from "react-native";
+import { SectionList, StyleSheet, View } from "react-native";
 import { AppText } from "../../ui/AppText";
 import { TransactionItem } from "./TransactionItem";
+import { useTheme } from "../../../hooks/useTheme";
 
 export function TransactionGroupList({transactions}) {
+    const { theme } = useTheme()
+    const styles = getStyles(theme)
     return (
         <SectionList 
             sections={transactions}
             keyExtractor={(item) => item.id.toString()}
             renderSectionHeader={({section}) => (
-                <AppText variant="headline" color="text">{section.header}</AppText>
+                <View style={styles.sectionHeader}>
+                    <AppText variant="headline">{section.header}</AppText>
+                </View>
             )}
             renderItem={({item}) => (
-                <TransactionItem transaction={item} />
+                <TransactionItem transaction={item} variant="dense"/>
+            )}
+            ItemSeparatorComponent={() => (
+                <View style={styles.separator}/>
             )}
         />
     )
 }
+
+const getStyles = (theme) => StyleSheet.create({
+    content: {
+        paddingHorizontal: theme.spacing.lg,
+        paddingBottom: theme.spacing.xl,
+    },
+    sectionHeader: {
+        marginBottom: theme.spacing.md,
+        marginTop: theme.spacing.lg
+    },
+    separator: {
+        height: theme.spacing.sm,
+    }
+})
