5ec2324f7c35bf9797b655e230d17a4f674e57ff
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-12 11:12:39 -0600

    refactor(hooks): update path imports into useHomeSummary

--- src/hooks/useHomeSummary.js
+++ src/hooks/useHomeSummary.js
@@ -1,12 +1,12 @@
 import { useMemo } from "react"
 import { transactionsService } from "../services/transactionsService.js"
-import { budgetsService } from "../services/budgetsServices.js"
+import { budgetsService } from "../features/budgets/services/budgetsServices.js"
 import { calculateIncomes } from "../utils/transactions/calculateIncomes.js"
 import { calculateExpenses } from "../utils/transactions/calculateExpenses.js"
 import { calculateBalance } from "../utils/transactions/calculateBalance.js"
 import { sortTransactionsByDate } from "../utils/transactions/sortTransactionsByDate.js"
 import { formatCurrency } from "../utils/formatters/formatCurrency.js"
-import { mapBudgetsWithMetrics } from "../utils/budgets/mapBudgetsWithMetrics.js"
+import { mapBudgetsWithMetrics } from "../features/budgets/utils/mapBudgetsWithMetrics.js"
 
 export const useHomeSummary = () => {
     const transactions = transactionsService().getTransactions()
