64d6d7cf85f04b78ca2d523c9568d21a71bb2a8d
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-12 10:59:59 -0600

    feat(budgets): add budgets screen and tab

--- src/app/navigation/TabsNavigator.jsx
+++ src/app/navigation/TabsNavigator.jsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { HomeScreen } from "../screens/HomeScreen"
 import { TransactionsScreen } from "../screens/TransactionsScreen"
+import { BudgetsScreen } from "../../features/budgets/screens/BudgetsScreen"
 
 const Tab = createBottomTabNavigator()
 
@@ -9,7 +10,7 @@ export function TabsNavigator() {
         <Tab.Navigator screenOptions={{headerShown: false}}>
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Transactions" component={TransactionsScreen} />
-            
+            <Tab.Screen name="Budgets" component={BudgetsScreen}/>
         </Tab.Navigator>
     )
 }
---
+++ src/features/budgets/screens/BudgetsScreen.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { AppText } from "../../../components/ui/AppText";
+
+export function BudgetsScreen() {
+    return (
+        <View>
+            <AppText variant="title">
+                Budgets Screen
+            </AppText>
+        </View>
+    )
+}
