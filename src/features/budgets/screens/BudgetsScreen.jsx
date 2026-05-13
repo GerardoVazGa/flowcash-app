b4b8d5d0a03476b43c171ab95ceb5ab5fe7b9534
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-13 14:01:58 -0600

    feat(budgets): add dynamic styles, theme state and use scroll view

--- src/features/budgets/screens/BudgetsScreen.jsx
+++ src/features/budgets/screens/BudgetsScreen.jsx
@@ -1,12 +1,20 @@
-import { View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { AppText } from "../../../components/ui/AppText";
+import { BudgetGlobalSummary } from "../components/BudgetGloabalSummary/BudgetGlobalSummary";
+import { useTheme } from "../../../hooks/useTheme";
 
 export function BudgetsScreen() {
+    const { theme } = useTheme()
+    const styles = getStyles(theme)
     return (
-        <View>
-            <AppText variant="title">
-                Budgets Screen
-            </AppText>
-        </View>
+        <ScrollView style = {styles.container}>
+            <BudgetGlobalSummary />
+        </ScrollView>
     )
 }
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        padding: theme.spacing.md
+    }
+})
