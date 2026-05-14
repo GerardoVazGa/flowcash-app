bcbec2950e4cd42141903ad7a8df0623b7beefe9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-14 13:42:14 -0600

    feat(budgets): add budgets status and variants constant for mapping data

---
+++ src/features/budgets/constants/budgetVariants.js
@@ -0,0 +1,10 @@
+import { BUDGET_STATUS } from "./budgetStatus";
+
+export const BUDGET_STATUS_VARIANTS = {
+    [BUDGET_STATUS.HEALTHY]: "safe",
+    [BUDGET_STATUS.WARNING]: "warning",
+    [BUDGET_STATUS.CRITICAL]: "caution",
+    [BUDGET_STATUS.EXCEEDED]: "danger",
+    [BUDGET_STATUS.LIMIT_REACHED]: "danger",
+    [BUDGET_STATUS.AT_RISK]: "danger"
+}
