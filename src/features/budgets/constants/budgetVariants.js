1be19b4baebffceb775bec507c1a37bb56964762
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-14 13:57:47 -0600

    refactor(budgets): delete redundant budget status value

--- src/features/budgets/constants/budgetVariants.js
+++ src/features/budgets/constants/budgetVariants.js
@@ -5,6 +5,5 @@ export const BUDGET_STATUS_VARIANTS = {
     [BUDGET_STATUS.WARNING]: "caution",
     [BUDGET_STATUS.CRITICAL]: "warning",
     [BUDGET_STATUS.EXCEEDED]: "danger",
-    [BUDGET_STATUS.LIMIT_REACHED]: "danger",
     [BUDGET_STATUS.AT_RISK]: "danger"
 }
