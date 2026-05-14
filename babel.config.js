dd83c6c607b7e4b08196e2edbf000589570bdecc
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-14 11:31:15 -0600

    chore: add babel import path aliases for Expo project

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@app': './src/app',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@context': './src/context',
+            '@data': './src/data',
+            '@features': './src/features',
+            '@hooks': './src/hooks',
+            '@services': './src/services',
+            '@utils': './src/utils'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
   };
 };
