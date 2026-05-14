e118acc017cd4b6d343564d26639e9e3e55f38ab
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-14 13:53:26 -0600

    refactor(budgets): change variant value on constants

--- src/features/budgets/constants/budgetVariants.js
+++ src/features/budgets/constants/budgetVariants.js
@@ -2,8 +2,8 @@ import { BUDGET_STATUS } from "./budgetStatus";
 
 export const BUDGET_STATUS_VARIANTS = {
     [BUDGET_STATUS.HEALTHY]: "safe",
-    [BUDGET_STATUS.WARNING]: "warning",
-    [BUDGET_STATUS.CRITICAL]: "caution",
+    [BUDGET_STATUS.WARNING]: "caution",
+    [BUDGET_STATUS.CRITICAL]: "warning",
     [BUDGET_STATUS.EXCEEDED]: "danger",
     [BUDGET_STATUS.LIMIT_REACHED]: "danger",
     [BUDGET_STATUS.AT_RISK]: "danger"
