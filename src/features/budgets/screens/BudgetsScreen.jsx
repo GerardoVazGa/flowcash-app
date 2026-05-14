2f01d931134342615511d40cf13b11f41dafe42e
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-14 10:44:00 -0600

    feat(budgets): render budgets into screen

--- src/features/budgets/screens/BudgetsScreen.jsx
+++ src/features/budgets/screens/BudgetsScreen.jsx
@@ -1,20 +1,50 @@
-import { ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { AppText } from "../../../components/ui/AppText";
-import { BudgetGlobalSummary } from "../components/BudgetGloabalSummary/BudgetGlobalSummary";
+import { BudgetGlobalSummary } from "../components/BudgetGlobalSummary/BudgetGlobalSummary";
 import { useTheme } from "../../../hooks/useTheme";
+import { useBudgets } from "../hooks/useBudgets";
+import { BudgetItem } from "../components/BudgetItem";
 
 export function BudgetsScreen() {
     const { theme } = useTheme()
     const styles = getStyles(theme)
+    const { budgets } = useBudgets()
     return (
-        <ScrollView style = {styles.container}>
-            <BudgetGlobalSummary />
-        </ScrollView>
+        <View style = {styles.container}>
+
+            <FlatList 
+                data={budgets}
+                keyExtractor={(item) => item.id.toString()}
+                ListHeaderComponent={() => (
+                    <View style = {styles.header}>
+                        <BudgetGlobalSummary />
+                    </View>
+                )}
+                renderItem={({item}) => (
+                    <BudgetItem 
+                        budget={item}
+                        showDelete = {false}
+                    />
+                )}
+                ItemSeparatorComponent={() => <View style={{height: theme.spacing.md}}/>}
+                contentContainerStyle={styles.items}
+
+            />
+
+        </View>
     )
 }
 
 const getStyles = (theme) => StyleSheet.create({
     container: {
-        padding: theme.spacing.md
+        flex: 1,
+        padding: theme.spacing.md,
+        paddingBottom: theme.spacing.lg,
+    },
+    header: {
+        marginBottom: theme.spacing.md
+    },
+    items: {
+        gap: theme.spacing.sm
     }
 })
