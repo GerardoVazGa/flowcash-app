4c37bed47109111797b589906208aeb21e099261
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-15 18:27:29 -0600

    feat(budgets): add budget filtering  utility for period, status and category

---
+++ src/features/budgets/utils/filters/filterBudgets.js
@@ -0,0 +1,30 @@
+import { matchesCategories } from "@utils/filters/matchesCategories"
+import { matchesPeriod } from "./matchesPeriod"
+import { matchesStatus } from "./matchesStatus"
+
+export function filterBudgets(budgets, filters) {
+    const {
+        period,
+        status,
+        category: categories,
+    } = filters
+    
+    const filteredBudgets = budgets.filter(budget => {
+        if(!matchesPeriod(budget.period, period)){
+            return false
+        }
+
+        if(!matchesCategories(budget.category, categories)) {
+            return false
+        }
+
+        if(!matchesStatus(budget.status, status)) {
+            return false
+        }
+
+
+        return true
+    })
+
+    return filteredBudgets
+}
