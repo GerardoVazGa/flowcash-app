cf6e9a98a6c0e4d7abc6d61916ff1fdbe0e4c638
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-19 12:10:44 -0600

    feat(layout-components): add filter section to use in each filter sheet

---
+++ src/components/layouts/FilterSection.jsx
@@ -0,0 +1,20 @@
+import { useTheme } from "@hooks/useTheme"
+import { StyleSheet } from "react-native"
+
+export function FilterSection({title, children}) {
+    const { theme } = useTheme()
+    const styles = getStyles(theme)
+
+    return (
+        <View style={styles.container}>
+            <AppText variant="label" color="textVariant">{title}</AppText>
+            {children}
+        </View>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        gap: theme.spacing.md
+    }
+})
