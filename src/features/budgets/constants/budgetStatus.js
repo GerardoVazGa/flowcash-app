47688cecaf2678ad501a8d8699c1a7abaf6dd94d
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-19 13:04:28 -0600

    feat(budgets): add Budget status config like label or icon,  and option to use in filters

--- src/features/budgets/constants/budgetStatus.js
+++ src/features/budgets/constants/budgetStatus.js
@@ -5,3 +5,28 @@ export const BUDGET_STATUS = {
     EXCEEDED: "EXCEEDED",
     AT_RISK: "AT_RISK"
 }
+
+export const BUDGET_STATUS_CONFIG = {
+    [BUDGET_STATUS.HEALTHY] : {
+        label: "Normal",
+        icon: "checkmark-circle-outline"
+    },
+    [BUDGET_STATUS.WARNING] : {
+        label: "Alerta",
+        icon: "alert-circle-outline"
+    },
+    [BUDGET_STATUS.CRITICAL] : {
+        label: "Crítico",
+        icon: "warning-outline"
+    },
+    [BUDGET_STATUS.AT_RISK] : {
+        label: "En riesgo",
+        icon: "trending-up-outline"
+    },
+    [BUDGET_STATUS.EXCEEDED] : {
+        label: "Excedido",
+        icon: "close-circle-outline"
+    },
+}
+
+export const BUDGET_STATUS_OPTIONS = Object.entries(BUDGET_STATUS_CONFIG).map(([value, config]) => ({ value, ...config }))
