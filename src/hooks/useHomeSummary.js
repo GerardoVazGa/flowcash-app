6e2755221b97893cb4acffaab06ec4383204f625
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-20 13:30:09 -0600

    refactor(hook): update import paths of home summary

--- src/hooks/useHomeSummary.js
+++ src/hooks/useHomeSummary.js
@@ -1,12 +1,12 @@
 import { useMemo } from "react"
-import { transactionsService } from "../services/transactionsService.js"
-import { budgetsService } from "../features/budgets/services/budgetsServices.js"
-import { calculateIncomes } from "../utils/transactions/calculateIncomes.js"
-import { calculateExpenses } from "../utils/transactions/calculateExpenses.js"
-import { calculateBalance } from "../utils/transactions/calculateBalance.js"
-import { sortTransactionsByDate } from "../utils/transactions/sortTransactionsByDate.js"
+import { transactionsService } from "@features/transactions/services/transactionsService.js"
+import { budgetsService } from "@features/budgets/services/budgetsServices.js"
+import { calculateIncomes } from "@features/transactions/utils/calculations/calculateIncomes.js"
+import { calculateExpenses } from "@features/transactions/utils/calculations/calculateExpenses.js"
+import { calculateBalance } from " @features/transactions/utils/calculations/calculateBalance.js"
+import { sortTransactionsByDate } from "@features/transactions/utils/sorting/sortTransactionsByDate.js"
 import { formatCurrency } from "../utils/formatters/formatCurrency.js"
-import { mapBudgetsWithMetrics } from "../features/budgets/utils/metrics/mapBudgetsWithMetrics.js"
+import { mapBudgetsWithMetrics } from "@features/budgets/utils/metrics/mapBudgetsWithMetrics.js"
 
 export const useHomeSummary = () => {
     const transactions = transactionsService().getTransactions()
