6a7693a48b8590968465cb3c5373e34454576461
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-20 11:37:16 -0600

    feat(budgets): add budget filters context to filter components and implement handlers to action and clean buttons

--- src/features/budgets/components/budgetFilters/BudgetFilterSheet.jsx
+++ src/features/budgets/components/budgetFilters/BudgetFilterSheet.jsx
@@ -1,23 +1,51 @@
 import { CategoryOption } from "@components/financial/filters/CategoryOption";
 import { FilterOption } from "@components/financial/filters/FilterOption";
 import { FilterSection } from "@components/layouts/FilterSection";
+import { AppButton } from "@components/ui/AppButton";
 import { AppText } from "@components/ui/AppText";
 import { CATEGORY_OPTIONS } from "@constants/filters";
 import { BUDGET_SORT_OPTIONS } from "@features/budgets/constants/budgetSort";
 import { BUDGET_STATUS_OPTIONS} from "@features/budgets/constants/budgetStatus";
+import { useFiltersBudgetContext } from "@features/budgets/context/FiltersBudgetContext";
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { useTheme } from "@hooks/useTheme";
 import { Pressable, StyleSheet, View } from "react-native";
 
-export function BudgetFilterSheet() {
+export function BudgetFilterSheet({ onClose = () => { } }) {
     const { theme } = useTheme()
     const styles = getStyles(theme)
 
-    return(
+    const {
+        draftFilters,
+        updateStatus,
+        updateSortBy,
+        toggleCategory,
+        cleanFilters,
+        applyFilters,
+        cancelFilters
+    } = useFiltersBudgetContext()
+
+    const handleApplyFilters = () => {
+        applyFilters()
+        onClose()
+    }
+
+    const handleCancelFilters = () => {
+        cancelFilters()
+        onClose()
+    }
+
+    const handleCleanFilter = () => {
+        cleanFilters()
+    }
+
+    return (
         <View style={styles.wrapper}>
             <View style={styles.header}>
                 <AppText variant="title">Filtrar Presupuestos</AppText>
-                <Pressable >
+                <Pressable
+                    onPress={handleCleanFilter}
+                >
                     <AppText variant="title" color="error">Limpiar</AppText>
                 </Pressable>
             </View>
@@ -29,25 +57,27 @@ export function BudgetFilterSheet() {
             >
                 <FilterSection title="Estado">
                     {
-                        BUDGET_STATUS_OPTIONS.map(({value, label, icon}) => (
-                            <FilterOption 
-                                key={value}
+                        BUDGET_STATUS_OPTIONS.map(({ value: status, label, icon }) => (
+                            <FilterOption
+                                key={status}
                                 title={label}
                                 iconName={icon}
-                                selected={value === "HEALTHY"}
+                                selected={status === draftFilters.status}
+                                onPress={() => updateStatus(status)}
                             />
                         ))
                     }
                 </FilterSection>
-                
+
                 <FilterSection title="Orden">
                     {
-                        BUDGET_SORT_OPTIONS.map(({value, label, icon}) => (
-                            <FilterOption 
-                                key={value}
+                        BUDGET_SORT_OPTIONS.map(({ value: sortby, label, icon }) => (
+                            <FilterOption
+                                key={sortby}
                                 title={label}
                                 iconName={icon}
-                                selected={value === "closest_to_limit"}
+                                selected={sortby === draftFilters.sortBy}
+                                onPress={() => updateSortBy(sortby)}
                             />
                         ))
                     }
@@ -56,17 +86,41 @@ export function BudgetFilterSheet() {
 
                 <FilterSection title="Categoría">
                     {
-                        CATEGORY_OPTIONS.map(({value, label}) => (
-                            <CategoryOption 
-                                key={value}
+                        CATEGORY_OPTIONS.map(({ value: category, label }) => (
+                            <CategoryOption
+                                key={category}
                                 title={label}
-                                selected={true}
-                                
+                                selected={draftFilters.category.includes(category)}
+                                onPress={() => toggleCategory(category)}
                             />
                         ))
                     }
 
                 </FilterSection>
+
+                <View style={styles.actions}>
+                    <AppButton
+                        onAction={handleCancelFilters}
+                        size="md"
+                        variant="outline"
+                        rounded="lg"
+                        backgroundColor="error"
+                        style={{ flex: 1 }}
+                    >
+                        <AppText variant="title" color="error">Cancelar</AppText>
+                    </AppButton>
+                    <AppButton
+                        onAction={handleApplyFilters}
+                        size="md"
+                        rounded="lg"
+                        fullwidth
+                        variant="gradient"
+                        style={{ flex: 2 }}
+                    >
+                        <AppText variant="title" color="onPrimary">Aplicar</AppText>
+                    </AppButton>
+
+                </View>
 
             </BottomSheetScrollView>
 
@@ -92,5 +146,11 @@ const getStyles = (theme) => StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
         marginBottom: theme.spacing.md
+    },
+    actions: {
+        flexDirection: "row",
+        alignItems: "center",
+        alignSelf: "stretch",
+        gap: theme.spacing.sm
     }
 })
