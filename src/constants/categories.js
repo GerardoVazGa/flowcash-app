fd71419470270838a8e5bc91b543bd432aa8875a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-21 11:06:13 -0600

    refactor(constants): merge category options and icons into global categories constant

---
+++ src/constants/categories.js
@@ -0,0 +1,57 @@
+
+export const CATEGORIES = {
+    trabajo: "trabajo",
+    alimentacion: "alimentación",
+    transporte: "transporte",
+    entretenimiento: "entretenimiento",
+    servicios: "servicios",
+    salud: "salud",
+    ventas: "ventas",
+    transferencia: "transferencia",
+}
+
+export const CATEGORY_OPTIONS = [
+    {
+        label: "Trabajo",
+        value: CATEGORIES.trabajo,
+    },
+    {
+        label: "Alimentación",
+        value: CATEGORIES.alimentacion,
+    },
+    {
+        label: "Transporte",
+        value: CATEGORIES.transporte,
+    },
+    {
+        label: "Entretenimiento",
+        value: CATEGORIES.entretenimiento,
+    },
+    {
+        label: "Servicios",
+        value: CATEGORIES.servicios,
+    },
+    {
+        label: "Salud",
+        value: CATEGORIES.salud,
+    },
+    {
+        label: "Ventas",
+        value: CATEGORIES.ventas,
+    },
+    {
+        label: "Transferencia",
+        value: CATEGORIES.transferencia,
+    },
+]
+
+export const CATEGORY_ICONS = {
+    [CATEGORIES.trabajo]: "briefcase-outline",
+    [CATEGORIES.alimentacion]: "fast-food-outline",
+    [CATEGORIES.transporte]: "car-outline",
+    [CATEGORIES.entretenimiento]: "film-outline",
+    [CATEGORIES.servicios]: "wifi-outline",
+    [CATEGORIES.salud]: "medical-outline",
+    [CATEGORIES.ventas]: "cash-outline",
+    [CATEGORIES.transferencia]: "swap-horizontal-outline",
+}
