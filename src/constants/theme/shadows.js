9984debd4ca7a1820cbde61a43643a6d9bff67a3
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-21 10:37:09 -0600

    refactor(constants): organize theme constants into theme folder

---
+++ src/constants/theme/shadows.js
@@ -0,0 +1,17 @@
+export const SHADOWS = {
+    light: {
+        shadowColor: "#000",
+        shadowOpacity: 0.06,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 2 },
+        elevation: 2,
+    },
+
+    dark: {
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        shadowOffset: { width: 0, height: 3 },
+        elevation: 3,
+    },
+}
