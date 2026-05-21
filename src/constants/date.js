bc0bcea8551e7d502ffd894b3eb24a20d7901908
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-21 11:42:27 -0600

    refactor(constants): extract shared date constants

---
+++ src/constants/date.js
@@ -0,0 +1,7 @@
+export const MONTHS_SHORT = [
+    "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"
+]
+
+export const MONTHS_FULLNAME = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+]
