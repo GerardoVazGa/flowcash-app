94146a26a0df96d3df34a2ae99de325b6aed6776
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-26 14:18:03 -0600

    feat(ui): add AppSelect component for usage as an input

---
+++ src/components/ui/AppSelect.jsx
@@ -0,0 +1,42 @@
+import { useTheme } from "@hooks/useTheme"
+import { Pressable, StyleSheet } from "react-native"
+import { AppIcon } from "./AppIcon"
+import { AppText } from "./AppText"
+
+export function AppSelect({
+    value,
+    onPress,
+    placeholder,
+    icon
+}) {
+    const { theme } = useTheme()
+    const styles = getStyles(theme)
+    return (
+        <Pressable 
+            onPress={onPress}
+            style={styles.container}
+        >
+            <View>
+                {icon && <AppIcon name={icon} size={18} color="textVariant" background="surfaceLow"/>}
+                <AppText variant="body">{value || placeholder}</AppText>
+
+                <AppIcon name="chevron-down" size={18} color="textVariant" />
+            </View>
+        </Pressable>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        gap: theme.spacing.sm,
+        backgroundColor: theme.colors.surface,
+        borderWidth: 1,
+        borderRadius: theme.radius.xl,
+        paddingHorizontal: theme.spacing.md,
+        paddingVertical: theme.spacing.sm
+    }
+})
