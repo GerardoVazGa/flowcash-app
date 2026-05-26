3c146470f4a829e24f3ac644ba5b60ff1593371e
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-26 13:28:58 -0600

    feat(forms): add SelectorBottomSheet component to choose anyoption into a modal

---
+++ src/components/forms/selectors/SelectorBottomSheet.jsx
@@ -0,0 +1,41 @@
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useTheme } from "@hooks/useTheme";
+import { useMemo } from "react";
+import { FlatList } from "react-native";
+import { SelectOption } from "./selectOption";
+
+export function SelectorBottomSheet({sheetRef, options, selected, onSelect}) {
+    const snapPoints = useMemo(() => ["50%", "75%"], [])
+    const {theme} = useTheme()
+    const styles = getStyles(theme)
+
+    const handleDismiss = (value) => {
+        onSelect(value)
+        sheetRef.current?.dismiss()
+    }
+
+    return (
+        <BottomSheetModal
+            ref={sheetRef}
+            snapPoints={snapPoints}
+            style={styles.container}
+        >
+            <FlatList 
+                data = {options}
+                keyExtractor={(item) => item.value}
+                renderItem={({item}) => (
+                    <SelectOption 
+                        label={item.label}
+                        icon={item.icon}
+                        selected={item.value === selected}
+                        onPress={() => handleDismiss(item.value)}
+                    />
+                )}
+            />
+        </BottomSheetModal>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({
+
+})
