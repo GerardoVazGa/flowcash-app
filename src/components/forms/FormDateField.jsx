361c4147a310806cff19043d5a23015fa0c7d2f1
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-05-27 12:22:21 -0600

    feat(forms): add FormDateField component for date selecting

---
+++ src/components/forms/FormDateField.jsx
@@ -0,0 +1,61 @@
+import { useFormField } from "@hooks/forms/useFormField"
+import { FormField } from "./FormField"
+import { AppSelect } from "@components/ui/AppSelect"
+import { useTheme } from "@hooks/useTheme"
+import { CalendarBottomSheet } from "./selectors/CalendarBottomSheet"
+import { useMemo } from "react"
+import { StyleSheet } from "react-native"
+
+export function FormDateField({
+    label,
+    name,
+    control,
+    placeholder = "Seleccione una fecha",
+}) {
+    const calendarSheetRef = useRef(null)
+
+    const { value, onChange, onBlur, error } = useFormField(control, name)
+
+    const { theme } = useTheme()
+    const styles = getStyles(theme)
+
+    const formattedDate = useMemo(() => {
+        if(!value) return ""
+
+        return Intl.DateTimeFormat("es-MX", {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        }).format(new Date(value))
+    }, [value])
+
+    const displayDateValue = useMemo(() => {
+        return formattedDate || placeholder
+    }, [formattedDate, placeholder])
+
+    const openSheet = () => calendarSheetRef.current?.present()
+
+    return(
+        <FormField
+            label={label}
+            error={error}
+        >
+            <AppSelect 
+                value={displayDateValue}
+                placeholder={placeholder}
+                disabled={disabled}
+                onPress={() => {}}
+                icon="calendar-outline"
+            />
+
+            <CalendarBottomSheet 
+                sheetRef={calendarSheetRef}
+                selectedDate={value}
+                onSelectDay={onChange}
+            />
+
+        </FormField>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({})
