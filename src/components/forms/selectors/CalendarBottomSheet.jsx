8632c267930c630c0a55dc1e4bc5709dbae9f0aa
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-08 13:26:56 -0600

    feat(forms): add date constraints, marked range, and custom arrow render into CalendarBottomSheet component

--- src/components/forms/selectors/CalendarBottomSheet.jsx
+++ src/components/forms/selectors/CalendarBottomSheet.jsx
@@ -1,7 +1,10 @@
+import { AppIcon } from "@components/ui/AppIcon";
 import { AppText } from "@components/ui/AppText";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { useTheme } from "@hooks/useTheme";
-import App from "App";
+import { generateMarkedDates } from "@utils/date/generateMarkedDates";
+import { getMinDateString } from "@utils/date/getMinDateString";
+import { getTodayString } from "@utils/date/getToday";
 import { useMemo } from "react";
 import { StyleSheet, View } from "react-native";
 import { Calendar } from "react-native-calendars";
@@ -9,11 +12,37 @@ import { Calendar } from "react-native-calendars";
 export function CalendarBottomSheet({
     sheetRef,
     selectedDate,
+    endDate,
+    allowFutureDates = false,
     onSelectDay
 }) {
-    const snapPoints = useMemo(() => ["50%", "75%"], [])
+    const snapPoints = useMemo(() => ["75%"], [])
     const { theme } = useTheme()
     const styles = getStyles(theme)
+
+    const maxDate = useMemo(() => {
+        if(allowFutureDates) return undefined
+
+        return getTodayString()
+    }, [allowFutureDates])
+
+    const minDate = useMemo(() => getMinDateString(), [])
+
+    const markedDates = useMemo(() => {
+        if(!selectedDate) return {}
+
+        if(!endDate) {
+            return {
+                [selectedDate]: {
+                    selected: true,
+                    disableTouchEvent: true,
+                }
+            }
+        }
+
+        return generateMarkedDates(selectedDate, endDate, theme)
+
+    },  [selectedDate, endDate, theme])
 
     const handleSelectDate = (day) => {
         onSelectDay(day.dateString)
@@ -25,13 +54,28 @@ export function CalendarBottomSheet({
             ref={sheetRef}
             snapPoints={snapPoints}
             enablePanDownToClose
+            enableDynamicSizing = {false}
+            stackBehavior="push"
         >
             <View style={styles.container}>
                 <AppText variant="title" color="text">Selecciona una fecha</AppText>
                 
                 <Calendar
-                    current={selectedDate}
+                    current={selectedDate || getTodayString()}
                     onDayPress={handleSelectDate}
+                    markedDates={markedDates}
+                    markingType={endDate ? "period" : "simple"}
+                    enableSwipeMonths = {true}
+                    hideExtraDays = {true}
+                    renderArrow={(direction) => (
+                        <AppIcon 
+                            name={direction === "left" ? "chevron-back" : "chevron-forward"}
+                            size={20}
+                            color={theme.colors.primary}
+                        />
+                    )}
+                    maxDate={maxDate}
+                    minDate={minDate}
                     theme={{
                         backgroundColor: theme.colors.surface,
                         calendarBackground:
@@ -42,14 +86,12 @@ export function CalendarBottomSheet({
                             theme.colors.text,
                         todayTextColor:
                             theme.colors.primary,
-                        arrowColor:
-                            theme.colors.primary,
                         selectedDayBackgroundColor:
                             theme.colors.primary,
                         selectedDayTextColor:
                             theme.colors.onPrimary,
                         textDisabledColor:
-                            theme.colors.textVariant
+                            `${theme.colors.textVariant}50`
                     }}
                 />
             </View>
