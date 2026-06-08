97777bf21f1896d5682d0a60e03d5ffd714ae4ef
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-08 12:22:05 -0600

    chore(utils): add getTodayString utility

--- src/utils/date/getToday.js
+++ src/utils/date/getToday.js
@@ -1,3 +1,13 @@
 export function getToday() {
     return new Date()
 }
+
+export function getTodayString() {
+    const today = getToday()
+
+    const year = today.getFullYear()
+    const month = String(today.getMonth() + 1).padStart(2, "0")
+    const day = String(today.getDate()).padStart(2, "0")
+
+    return `${year}-${month}-${day}`
+}
