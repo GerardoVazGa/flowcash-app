3579066ece79511221d445f215e7cbfe99c3fef0
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-09 08:42:03 -0600

    feat(accounts): add accounts mock data

---
+++ src/features/accounts/data/acoountsMock.js
@@ -0,0 +1,18 @@
+import { ACCOUNT_TYPE } from "../constants/accounts";
+
+export const accountsMock = [
+    {
+        id: 1,
+        name: "Cuenta 1",
+        type: ACCOUNT_TYPE.DEBIT,
+        balance: 0,
+        currency: "MXN",
+    },
+    {
+        id: 2,
+        name: "Cuenta 2",
+        type: ACCOUNT_TYPE.DEBIT,
+        balance: 0,
+        currency: "MXN",
+    },
+]
