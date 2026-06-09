382691dd1e46a7eaed45e97e1e8fe51feee973af
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-08 19:22:51 -0600

    chore(config): add keyboard controller provider and adjustResize for bottom sheet

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { ThemeProvider } from './src/context/ThemeContext';
 import { AppContent } from './src/app/AppContent.jsx';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <ThemeProvider>
-        <BottomSheetModalProvider>
-          <AppContent />
-        </BottomSheetModalProvider>
+        <KeyboardProvider>
+          <BottomSheetModalProvider>
+            <AppContent />
+          </BottomSheetModalProvider>
+        </KeyboardProvider>
       </ThemeProvider>
     </GestureHandlerRootView>
   );
