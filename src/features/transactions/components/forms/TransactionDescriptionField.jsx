482048b957cc4aa6618652443f1f82d4b5a1a606
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-08 20:12:56 -0600

    refactor(transactions): rename TransactionNoteField to TransactionDescriptionField

--- src/features/transactions/components/forms/TransactionDescriptionField.jsx
+++ src/features/transactions/components/forms/TransactionDescriptionField.jsx
@@ -2,13 +2,13 @@ import { FormTextArea } from "@components/forms/FormTextArea";
 import { useTheme } from "@hooks/useTheme";
 import { StyleSheet } from "react-native";
 
-export function TransactionNoteField({name}) {
+export function TransactionDescriptionField({name}) {
     const { theme } = useTheme()
     const styles = getStyles(theme)
     return (
         <FormTextArea 
             name={name}
-            label="Notas (Opcional)"
+            label="Descripción"
             placeholder="Añadir una descripción..."
             numberOfLines={4}
             style={styles.textarea}
