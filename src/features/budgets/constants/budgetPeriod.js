9a53de81810e2aefe8dfe488d7896d22d6ce7625
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-09 18:33:48 -0600

    feat(budgets): add period icons and update BUDGETS_PERIODS_OPTIONS

--- src/features/budgets/constants/budgetPeriod.js
+++ src/features/budgets/constants/budgetPeriod.js
@@ -12,4 +12,15 @@ export const BUDGETS_PERIODS_LABELS = {
     [BUDGETS_PERIODS.YEARLY]: "Anual"
 }
 
-export const BUDGETS_PERIODS_OPTIONS = Object.entries(BUDGETS_PERIODS_LABELS).map(([value, label]) => ({ value, label }))
+export const BUDGETS_PERIODS_ICONS = {
+    [BUDGETS_PERIODS.WEEKLY]: "calendar-outline",
+    [BUDGETS_PERIODS.BIWEEKLY]: "git-branch-outline",
+    [BUDGETS_PERIODS.MONTHLY]: "calendar-number-outline",
+    [BUDGETS_PERIODS.YEARLY]: "infinite-outline",
+}
+
+export const BUDGETS_PERIODS_OPTIONS = Object.entries(BUDGETS_PERIODS_LABELS).map(([value, label]) => ({
+    value, 
+    label,
+    icon: BUDGETS_PERIODS_ICONS[value]
+}))
