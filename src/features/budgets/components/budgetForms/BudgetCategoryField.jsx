9dc9f56162b72c2fc0f373427ad26f38bffdb1ee
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-06-10 18:59:20 -0600

    fix(budgets): fix usage of categories map change from CATEGORIES to CATEGORY_OPTION constant

--- src/features/budgets/components/budgetForms/BudgetCategoryField.jsx
+++ src/features/budgets/components/budgetForms/BudgetCategoryField.jsx
@@ -1,9 +1,9 @@
 import { FormSelectField } from "@components/forms/FormSelectField";
-import { CATEGORIES, CATEGORY_ICONS } from "@constants/categories";
+import { CATEGORY_ICONS, CATEGORY_OPTIONS } from "@constants/categories";
 
 export function BudgetCategoryField({ name }) {
 
-    const categories = CATEGORIES.map(category => ({
+    const categories = CATEGORY_OPTIONS.map(category => ({
         ...category,
         icon: CATEGORY_ICONS[category.value]
     }))
